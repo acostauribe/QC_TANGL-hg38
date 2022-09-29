5a1af52743caab8f793d9cc2d58a67ae455d7064
Juliana Acosta-Uribe <52937200+acostauribe@users.noreply.github.com>
2022-09-29 11:55:44 -0700

    Update autosomal_pre_QC_commands.sh

--- autosomal_pre_QC_commands.sh
+++ autosomal_pre_QC_commands.sh
@@ -3,41 +3,33 @@
 
 # This script is an overview of the pre-quality control commands used to analyze the dataset. 
 
-# STEP 0: SET UP DATA AND VARIABLES 
-# STEP 1: SOFT QC
-# STEP 2: GENERAL REPORT OF PRE QC STATISTICS
+# STEP -1: CHECK VCF FILE, 
+# STEP -0: SET UP DATA AND VARIABLES 
+# STEP 1.a: SOFT QC
+# STEP 1.b: GENERAL REPORT OF PRE QC STATISTICS
 
 
+## STEP -1: CHECK VCF FILE===================================================================
 ## STEP 0: SET UP DATA AND VARIABLES===================================================================
 
 ## Files with the data you want to QC
 PREFIX_raw = 'joint_colombia_annotated.hg38' #PREFIX of the raw dataset. Should be a vcf.gz
 PREFIX_autosomes = 'joint_colombia_annotated.hg38.autosomes' #PREFIX of the dataset with only autosome data. 
 
-## Separate the raw dataset into autosomes and sex chromosomes
 
-### Extract autosomes
-vcftools --gzvcf $PREFIX_raw.vcf.gz --chr chr1 --chr chr2 --chr chr3 --chr chr4 --chr chr5 --chr chr6 --chr chr7 --chr chr8 --chr chr9 --chr chr10 --chr chr11 --chr chr12 --chr chr13 --chr chr14 --chr chr15 --chr chr16 --chr chr17 --chr chr18 --chr chr19 --chr chr20 --chr chr21 --chr chr22 --recode --out $PREFIX_autosomes
-bgzip $PREFIX_autosomes.recode.vcf
-mv $PREFIX_autosomes.recode.vcf.gz $PREFIX_autosomes.vcf.gz
+## STEP 1.a: SOFT QC=====================================================================================
 
-### Extract chrX and chrY
-vcftools --gzvcf $PREFIX_raw.vcf.gz --chr chrX --recode --out $PREFIX_raw.chrX
-mv $PREFIX_raw.chrX.recode.vcf $PREFIX_raw.chrX.vcf
-bgzip $PREFIX_raw.chrX.vcf
+### Get rid of any sites and individuals with high missing data before we make a decision about QC thresholds.
 
-vcftools --gzvcf $PREFIX_raw.vcf.gz --chr chrY --recode --out $PREFIX_raw.chrY
-mv $PREFIX_raw.chrY.recode.vcf $PREFIX_raw.chrY.vcf
-bgzip $PREFIX_raw.chrY.vcf
+###Indentify individuals with missingness >10%
+awk '{$5>0.1 print$1}' $PREFIX_raw.imiss > individuals_highmissingness.txt
 
-## STEP 1: SOFT QC=====================================================================================
-### Get rid of any extreme outliers before we make a decision about QC thresholds.
 ### Missingness >= 10% ( --max-missing float is a 0-1range. 0 allows sites that are completely missing 
-###                      and 1 indicates no missing data allowed)
+### and 1 indicates no missing data allowed)
 
-vcftools --gzvcf $PREFIX_autosomes.vcf.gz --max-missing 0.9 --recode --recode-INFO-all --out $PREFIX_autosomes.vcf.gz.miss
-mv $PREFIX_autosomes.vcf.gz.miss $PREFIX_autosomes.miss.vcf
-bgzip $PREFIX_autosomes.miss.vcf
+vcftools --gzvcf $PREFIX_autosomes.vcf.gz --remove individuals_highmissingness.txt --max-missing 0.9 \
+--recode --recode-INFO-all --stdout | gzip -c > $PREFIX_autosomes.miss.vcf.gz
+
 
 ## STEP 2: GENERAL REPORT OF PRE QC STATISTICS OF AUTOSOMES============================================
 
@@ -73,3 +65,19 @@ vcftools --gzvcf $PREFIX_autosomes.miss.vcf.gz --depth --out $PREFIX_autosomes.m
 vcftools --gzvcf $PREFIX_autosomes.miss.vcf.gz --het --out $PREFIX_autosomes.miss.PRE-QC
 # Inbreeding coefficient, F, is estimated for each individual using a method of moments. 
 # The resulting file has the suffix ".het"
+
+
+
+
+## Separate the raw dataset into autosomes and sex chromosomes
+
+### Extract autosomes
+vcftools --gzvcf $PREFIX_raw.vcf.gz --chr chr1 --chr chr2 --chr chr3 \
+--chr chr4 --chr chr5 --chr chr6 --chr chr7 --chr chr8 --chr chr9 --chr \
+chr10 --chr chr11 --chr chr12 --chr chr13 --chr chr14 --chr chr15 --chr chr16 \
+--chr chr17 --chr chr18 --chr chr19 --chr chr20 --chr chr21 --chr chr22 \
+--recode --recode-info-all --stdout | gzip -c > $PREFIX_autosomes.vcf.gz
+
+### Extract chrX and chrY
+vcftools --gzvcf $PREFIX_raw.vcf.gz --chr chrX --recode --recode-info-all --stdout | gzip -c > $PREFIX_raw.chrX.vcf.gz
+vcftools --gzvcf $PREFIX_raw.vcf.gz --chr chrY --recode-info-all --stdout | gzip -c > $PREFIX_raw.chrY.vcf.gz
